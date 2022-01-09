fb2067fa24bcff200cdace04acb6b7e9ab407ce3
Michael Hackett <87140276+MikeCHackett@users.noreply.github.com>
2022-01-09 15:07:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function Spa() {
   return (
     <HashRouter>
       <NavBar/>
-      <UserContext.Provider value={{users:[{name:'Michael',email:'mike@gmail.com',password:'secret123',balance:50}]}}>
-        <div className="container" style={{padding: "20px"}}>
+      <UserContext.Provider value={{users:[{id: 1, name:'Michael',email:'mike@gmail.com',password:'secret123',balance:50}]}}>
+        <div className="container" style={{padding: "100px"}}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
           <Route path="/login/" component={Login} />
