e73a42b26465deb158b056546c826297cb7d2db0
Michael Hackett <87140276+MikeCHackett@users.noreply.github.com>
2022-01-09 15:08:47 -0500

    Update withdraw.js

--- withdraw.js
+++ withdraw.js
@@ -8,7 +8,7 @@ function Withdraw() {
 
   return (
       <Card
-        bgcolor="secondary"
+        bgcolor="primary"
         header="Withdraw"
         withdraw={handle}
         submitButtonWithdraw="Withdrawl Successful"
