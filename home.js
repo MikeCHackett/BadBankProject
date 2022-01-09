e156ad68a9956412257803959d8b1f96fd400289
Michael Hackett <87140276+MikeCHackett@users.noreply.github.com>
2022-01-09 15:06:59 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,8 +1,8 @@
 function Home(){
   return (
     <Card
-      bgcolor="secondary"
-      txtcolor="black"
+      bgcolor="primary"
+      txtcolor="light"
       header="BadBank Landing Module"
       title="Welcome to Bad Bank"
       text="Interact with the menu to begin!"
